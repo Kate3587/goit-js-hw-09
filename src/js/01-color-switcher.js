a5bdee66381f3335e0f4eb42f3301c5eeba656d5
Kate3587 <108617406+Kate3587@users.noreply.github.com>
2022-09-20 12:26:35 +0200

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
 
+const bodyEl = document.querySelector('body');
+const btnStart = document.querySelector('[data-start]');
+const btnStop = document.querySelector('[data-stop]');
+btnStop.disabled = true;
+
+btnStart.addEventListener('click', onStartCklick);
+
+let timerId;
+
+function onStartCklick(event) {
+    btnStart.disabled = true;
+    btnStop.disabled = false;
+    timerId = setInterval(() => {
+        const bodyColor = getRandomHexColor();
+        bodyEl.style.background = bodyColor;
+    }, 1000);
+};
+
+btnStop.addEventListener('click', onStopClick);
+
+function onStopClick(event) {
+    btnStart.disabled = false;
+    btnStop.disabled = true;
+    clearInterval(timerId);
+};
